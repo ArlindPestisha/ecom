ad69f0f09fee0900c9d8192acf61b08b1b9fb2a5
ArlindPestisha <arlind.pestisha@hyperisland.se>
2022-10-17 15:03:43 +0200

    Adding the category section!

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,7 +1,84 @@
-import React from 'react'
+import React from "react";
 
 const Category = () => {
   return (
-    <div>Category</div>
-  )
-}
+    <>
+      <div className=" flex flex-col md:flex-row justify-center mt-32 w-[1110px]  mb-10">
+        <div className=" w-[350px] overflow-hidden rounded-lg relative text-center">
+          <div className=" pt-8">
+            {" "}
+            <img
+              src="/public/assets/shared/desktop/image-category-thumbnail-headphones.png"
+              alt=""
+            />
+          </div>
+
+          <div className=" mb-3">
+            <h1 className=" uppercase text-lg font-bold tracking-[1.5px]">
+              Headphones
+            </h1>
+          </div>
+          <div className="flex justify-center">
+            <p>Shop</p>
+            <span className=" p-2">
+              <img
+                src="/public/assets/shared/desktop/icon-arrow-right.svg"
+                alt=""
+              />
+            </span>
+          </div>
+        </div>
+
+        <div className=" w-[350px] overflow-hidden rounded-lg relative text-center">
+          <div className=" pt-8">
+            {" "}
+            <img className="mb-2"
+              src="/public/assets/shared/desktop/image-category-thumbnail-speakers.png"
+              alt=""
+            />
+          </div>
+          <div className=" mb-3">
+            <h1 className=" uppercase text-lg font-bold tracking-[1.5px]">
+              Speakers
+            </h1>
+          </div>
+          <div className="flex justify-center">
+            <p>Shop</p>
+            <span className=" p-2">
+              <img
+                src="/public/assets/shared/desktop/icon-arrow-right.svg"
+                alt=""
+              />
+            </span>
+          </div>
+        </div>
+
+        <div className=" w-[350px] overflow-hidden rounded-lg relative text-center">
+          <div className=" pt-8">
+            {" "}
+            <img className=" mb-9"
+              src="/public/assets/shared/desktop/image-category-thumbnail-earphones.png"
+              alt=""
+            />
+          </div>
+          <div className=" mb-3">
+            <h1 className=" uppercase text-lg font-bold tracking-[1.5px]">
+              Earphones
+            </h1>
+          </div>
+          <div className="flex justify-center">
+            <p>Shop</p>
+            <span className=" p-2">
+              <img
+                src="/public/assets/shared/desktop/icon-arrow-right.svg"
+                alt=""
+              />
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Category;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Category from "../components/Category";
 
 const Home = () => {
   return (
-    <div className="flex justify-center w-full mx-auto ">
+    <div className="flex justify-center flex-wrap w-full mx-auto ">
       <Hero />
+      <Category />
     </div>
   );
 };
